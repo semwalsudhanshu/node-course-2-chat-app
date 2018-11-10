e1fd7631bb3e1e67fc1f72a430e78d8c8cd92f06
semwalSudhanshu <semwal.sudhanshu@gmail.com>
2018-11-10 18:28:50 +0530

    ADD FORM FOR MESSAGES

--- server/server.js
+++ server/server.js
@@ -26,9 +26,10 @@ io.on('connection',(socket)=>{
   // socket.on('CreateEmail',(newEmail)=>{
   //   console.log('createEmail',newEmail);
   // });
-  socket.on('createMessage',(newMessage)=>{
+  socket.on('createMessage',(newMessage,callback)=>{
     console.log('New Message Recieved from the client to server',newMessage);
     io.emit('newMessage',generateMessage(newMessage.from,newMessage.text));
+    callback('This is from the server');
     // socket.broadcast.emit('newMessage',{
     //   from:newMessage.from,
     //   text:newMessage.text,
