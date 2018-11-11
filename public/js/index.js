c5f178566d603cde58c081f83b6f3832503fc33b
semwalSudhanshu <semwal.sudhanshu@gmail.com>
2018-11-11 08:34:49 +0530

    SCROLL TO BOTTOM

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,18 @@
 var socket=io();
+
+function scrollToBottom () {
+  var messages =jQuery('#messages');
+  var newMessage =messages.children('li:last-child');
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight =messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight=newMessage.prev().innerHeight();
+  if(scrollTop+clientHeight+newMessageHeight+lastMessageHeight>=scrollHeight)
+  {
+    messages.scrollTop(scrollHeight);
+  }
+}
 socket.on('connect',function () {
   console.log('Connected to server');
   // socket.emit('CreateEmail',{
@@ -22,6 +36,7 @@ socket.on('newMessage',function (message){
       createdat:formattedTime
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 
   // console.log('New Message recieved. ',message);
   // var li =jQuery('<li></li>');
@@ -38,6 +53,7 @@ socket.on('newLocationMessage',function(message){
     createdat:formattedTime
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
   // var li=jQuery('<li></li>');
   // var a =jQuery('<a target = "_blank">My current location</a>');
   // li.text(message.from+' '+formattedTime+' : ');
